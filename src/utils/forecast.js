310f632df42c873bfbc4df57890799e6f9eb6003
david-asem <ddy.asem@gmail.com>
2021-03-26 20:23:54 +0000

    Add new information to forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out in ' + body.location.name + ', making it ' + body.current.weather_descriptions +'. Humidity is currently at ' +body.current.humidity+'.'+'There is  therefore  a ' + body.current.precip + '% chance of rain today.')
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out in ' + body.location.name + ', making it ' + body.current.weather_descriptions +'. It feels like '+body.current.feelslike+' degress outside though.'+ ' Humidity is currently at ' +body.current.humidity+'.'+'There is  therefore  a ' + body.current.precip + '% chance of rain today.')
         }
     })
 }
